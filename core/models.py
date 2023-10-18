5bf14914d82f3b6a5fea46c537e3c39590a2279c
stza <admin@seo-rocket.ru>
2023-10-18 23:36:36 +0400

    add domain status

--- core/models.py
+++ core/models.py
@@ -57,7 +57,7 @@ class SearchQuery(models.Model):
 CHOICE_DOMAIN_STATUS = (
     (0, 'added'),
     (1, 'check files'),
-    (2, 'inprogress')
+    (2, 'inprogress'),
     (3, 'get images'),
     (4, 'checked'),
     (5, 'get files')
