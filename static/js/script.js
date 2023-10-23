99f6af76556dddc9a43e37892fce5b7cd3812311
Festanny <60522731+Festanny@users.noreply.github.com>
2023-10-23 21:05:18 +0400

    test

--- static/js/script.js
+++ static/js/script.js
@@ -755,7 +755,7 @@ function showToasts(text, color) {
 
 
 let domain_main = window.location.hostname
-window.location.port ? domain_main += `:${window.location.port}` : domain_main += `:22`
+window.location.port ? domain_main += `:${window.location.port}` : domain_main += `:80`
 const socket = new WebSocket(`ws://${domain_main}/ws`);
 
 socket.onopen = function(e) {
