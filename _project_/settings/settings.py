20632e442d38d400268bdf5476d597679cc491f1
Festanny <60522731+Festanny@users.noreply.github.com>
2023-10-25 14:05:57 +0400

    test

--- _project_/settings/settings.py
+++ _project_/settings/settings.py
@@ -87,9 +87,18 @@ ASGI_APPLICATION = '_project_.asgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [("127.0.0.1", 6379)],
+        }
     }
 }
+
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels.layers.InMemoryChannelLayer'
+#     }
+# }
 
 
 # Database
